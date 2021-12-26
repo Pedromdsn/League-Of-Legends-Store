cfe797cf496d33c8a56d0844ee025ce39bfef2e1
pedromdsn <pedromdsn.2002@hotmail.com>
2021-12-26 20:02:59 +0000

    git: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>LOL Store</title>
+				<link rel="icon" type="image/icon" sizes="16x16" href="/favicon.ico" />
+				<meta name="description" content="League of Legends Accounts Store" />
+				<meta name="author" content="Coco Blanco" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
